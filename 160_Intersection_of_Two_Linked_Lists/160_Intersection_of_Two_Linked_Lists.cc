0cdcc82d98187247cd4ee49f09fafed314b2e4c5
wushangzhen <wushangzhen_bupt@163.com>
2018-04-26 17:58:58 +0800

    April 26th LC 2

---
+++ 160_Intersection_of_Two_Linked_Lists/160_Intersection_of_Two_Linked_Lists.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+  ListNode* p1 = headA;
+  ListNode* p2 = headB;
+  if (!headA || !headB) {
+    return NULL;
+  }
+  while (p1 != p2) {
+    p1 = p1->next;
+    p2 = p2->next;
+    if (p1 == p2) {
+      return p1;
+    }
+    if (p1 == NULL) {
+      p1 = headB;
+    }
+    if (p2 == NULL) {
+      p2 = headA;
+    }
+  }
+  return p1;
+}
---
+++ 160_Intersection_of_Two_Linked_Lists/note.md
@@ -0,0 +1,5 @@
+We have to find the common max length of two linked-list, so that we can compare the two linked list one by one
+
+When one pointer goes to the end, we switch it to another linked list's beginning
+
+In this way, when the longer linked finishes traverse, we can compare two list's element
