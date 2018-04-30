c9ff49e66ec79d18d694075ec688482c4bb7d731
wushangzhen <wushangzhen_bupt@163.com>
2018-04-30 14:35:38 +0800

    April 30th LC 1

---
+++ 96_Unique_Binary_Search_Trees/96_Unique_Binary_Search_Trees.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int numTrees(int n) {
+  int dp[n + 1] = {0}; // have to be initialized 
+  dp[0] = 1;
+  dp[1] = 1;
+  for (int i = 2; i < n + 1; i++) {
+    for (int j = 0; j < i; j++) {
+      dp[i] += dp[j] * dp[i - j - 1];
+    }
+  }
+  return dp[n];
+}
