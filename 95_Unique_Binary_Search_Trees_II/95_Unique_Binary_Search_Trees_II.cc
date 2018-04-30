9109feab22e3aac80e330129647cbb95a3fd875b
wushangzhen <wushangzhen_bupt@163.com>
2018-04-30 16:56:59 +0800

    April 30th LC 2

---
+++ 95_Unique_Binary_Search_Trees_II/95_Unique_Binary_Search_Trees_II.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+  vector<TreeNode*> generateTrees(int n) {
+    vector<TreeNode*> result_vec;
+    if (n < 1) {
+      return result_vec;
+    }
+    return helper(1, n);
+  }
+  vector<TreeNode*> helper(int start, int end) {
+    vector<TreeNode*> result_vec;
+    if (start > end) {
+      result_vec.push_back(NULL);
+      return result_vec;
+    }
+    for (int i = start; i <= end; i++) {
+      vector<TreeNode*> leftChild = helper(start, i - 1);
+      vector<TreeNode*> rightChild = helper(i + 1, end);
+      for (auto left : leftChild) {
+        for (auto right : rightChild) {
+          TreeNode* root = new TreeNode(i);
+          root->left = left;
+          root->right = right;
+          result_vec.push_back(root);
+        }
+      }
+    }
+    return result_vec;
+    
+  }
+};
