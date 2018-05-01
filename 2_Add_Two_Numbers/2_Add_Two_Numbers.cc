f06715cda57d384ab0df003f3c81cd33232f3285
wushangzhen <wushangzhen_bupt@163.com>
2018-05-02 00:02:14 +0800

    Updated

--- 2_Add_Two_Numbers/2_Add_Two_Numbers.cc
+++ 2_Add_Two_Numbers/2_Add_Two_Numbers.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 
-ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-  ListNode* p1 = l1;
+  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* p1 = l1;
   ListNode* p2 = l2;
   if (!l1 && l2) {
     return l2;
@@ -12,13 +12,47 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
   if (!l1 && !l2) {
     return NULL;
   }
-  while (p1->next && p2->next) {
+  int count = 0;
+  ListNode* pre;
+  while (p1 && p2) {
     p1->val += p2->val;
+    p1->val += count;
+    if (p1->val > 9) {
+      count = 1;
+      p1->val %= 10;
+    } else {
+      count = 0;
+    }
+    pre = p1;
     p1 = p1->next;
     p2 = p2->next;
   }
-  if (!p1) {
-    p1->next = p2;
+  while (p1) {
+    p1->val += count;
+    if (p1->val > 9) {
+      p1->val %= 10;
+      count = 1;
+    } else {
+      count = 0;
+    }
+    pre = p1;
+    p1 = p1->next;
   }
-  return p1;
-}
+  while (p2) {
+    pre->next = p2;
+    p2->val += count;
+    if (p2->val > 9) {
+      p2->val %= 10;
+      count = 1; 
+    } else {
+      count = 0;
+    }
+    pre = p2;
+    p2 = p2->next;
+  }
+  if (count == 1) {
+    ListNode* end = new ListNode(1);
+    pre->next = end;
+  }
+  return l1;
+    }
