0330db2e965829e5abf346cbe5834489f9719939
wushangzhen <wushangzhen_bupt@163.com>
2018-05-03 00:33:48 +0800

    May 2th LC 3 Updated

--- 6_ZigZag_Conversion/6_ZigZag_Conversion.cc
+++ 6_ZigZag_Conversion/6_ZigZag_Conversion.cc
@@ -1,16 +1,36 @@
 #include <iostream>
 #include <vector>
 #include <string>
+using namespace std;
 string convert(string s, int numRows) {
+  if (s.length() == 0 || numRows == 0) {
+      return "";
+  }
+  if (numRows == 1) {
+    return s;
+  }
   int n = 2 * numRows - 2;
-  vector<string> vec;
+  vector<string> vec(numRows, ""); // better initialize
   for (int i = 0; i < s.length(); i++) {
     int temp = i;
-    temp %= n
+    temp %= n;
     if (temp < numRows) { 
       vec[temp] += s[i];
     } else {
-      vec[numRows - n - temp]
+      vec[numRows - ((temp + 1) % numRows) - 1] += s[i];
     }
-  }      
+  }
+  if (vec.size() == 0) {
+      return "";
+  }
+  string result = "";
+  for (int i = 0; i < vec.size(); i++) {
+    result += vec[i];
+  }
+  return result;
+} 
+
+int main() {
+  string str = "PAYPALISHIRING";
+  std::cout << convert(str, 3) << std::endl;
 }
