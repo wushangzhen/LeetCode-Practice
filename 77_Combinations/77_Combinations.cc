8e69a3b01bca1de70f593b4511f8516a8544f82e
wushangzhen <wushangzhen_bupt@163.com>
2018-05-07 15:06:32 +0800

    May 7 LC 1

---
+++ 77_Combinations/77_Combinations.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+vector<vector<int> > combine(int n, int k) {
+  vector<vector<int> > vec_res;
+  vector<int> path;
+  helper(vec_res, path, 1, k);
+  return vec_res;
+}
+void helper(vector<vector<int> >& vec_res, vector<int>& path, int ix, int k) {
+  if (path.size() == k) {
+    vec_res.push_back(path);
+    return;
+  }
+  for (int i = ix; i <= n; i++) {
+    path.push_back(i);
+    helper(vec_res, path, i + 1, k);
+    path.erase(path.end() - 1);
+  }
+}
