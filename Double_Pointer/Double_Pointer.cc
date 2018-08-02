e6c7008d46e96d91d0e08007bbf9ec936baa5e1a
wushangzhen <wushangzhen_bupt@163.com>
2018-08-02 18:52:22 +0800

    Double Pointer Template

---
+++ Double_Pointer/Double_Pointer.cc
@@ -0,0 +1,17 @@
+void reverse(string s) {
+    int left = 0, right = s.length - 1;
+    while (left < right) {
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }
+}
+void reverse(string s) {
+    for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+}
