5ea037133178d4c0b46c74b9b1c9426af73de342
wushangzhen <wushangzhen_bupt@163.com>
2018-08-04 11:03:07 +0800

    8/4/2018

---
+++ 141_Linked_List_Cycle/8-4-2018.cc
@@ -0,0 +1,37 @@
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+
+
+class Solution {
+public:
+    /*
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    bool hasCycle(ListNode * head) {
+        // write your code here
+        if (head == NULL) {
+            return false;
+        }
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        while (fast != NULL && fast->next != NULL) {
+            if (slow == fast) {
+                return true;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return false;
+    }
+};
