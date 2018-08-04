dff58f93ed41c7e0efada0270a32c02cb2d12e85
wushangzhen <wushangzhen_bupt@163.com>
2018-08-04 11:06:05 +0800

    8/4/2018

---
+++ 228_Middle_of_Linked_List/8-4-2018-two-pointer.cc
@@ -0,0 +1,32 @@
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+
+
+class Solution {
+public:
+    /*
+     * @param head: the head of linked list.
+     * @return: a middle node of the linked list
+     */
+    ListNode * middleNode(ListNode * head) {
+        // write your code here
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+        
+    }
+};
